0810a2ae97b6c7ceb685a9ce2c590686a8b7d16c
F41zan <g24.faizan.khan@gnkhalse.edu.in>
2026-02-06 12:21:38 +0530

    router update

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter,HashRouter } from 'react-router-dom'
 import CardContextProvider from './component/Context/CardContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
    <CardContextProvider>
-    <BrowserRouter basename="/Barca-Shop">
+    <HashRouter>
     <App />
-    </BrowserRouter>
+    </HashRouter>
      </CardContextProvider > 
   // </StrictMode>,
 )
