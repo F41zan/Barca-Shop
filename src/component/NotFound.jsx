daab3859375a371bd2ebf112afd106bf5853b938
F41zan <g24.faizan.khan@gnkhalse.edu.in>
2026-04-04 16:03:49 +0530

    updates

--- src/component/NotFound.jsx
+++ src/component/NotFound.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div className='h-screen mt-40'>
-      <h1 className='text-6xl mt-18 pl-20 font-bold text-7xl'>404</h1>
-      <h4 className='mt-9 pl-18 text-2xl text-[#7d7d7d] font-normal'>The requested page was not found.</h4>
+    <div className="min-h-screen flex flex-col  sm:ml-20 sm:mt-64 items-center justify-center sm:items-start sm:justify-start  px-4">
+      
+      <h1 className="text-5xl sm:text-6xl md:text-7xl font-bold">
+        404
+      </h1>
+
+      <h4 className="mt-4 text-base sm:text-lg md:text-2xl text-gray-500">
+        The requested page was not found.
+      </h4>
+
     </div>
   )
 }
